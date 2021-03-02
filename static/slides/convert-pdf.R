497755eb7f78a1351bd2cbd6c3cdd572f92fe768
Daniel Anderson <daniela@uoregon.edu>
2021-03-02 15:07:50 -0800

    fix some links I accidentally broke

--- static/slides/convert-pdf.R
+++ static/slides/convert-pdf.R
@@ -27,11 +27,11 @@ no_pdfs <- anti_join(rmds, pdfs, by = "week")
 
 to_print <- anti_join(pdfs, html, by = c("week", "modification_time")) %>% 
   semi_join(rmds, ., by = "week") %>% 
-  filter(week != "w1p1") %>% 
+  #filter(week != "w1p1") %>% 
   bind_rows(no_pdfs)
 
 purrr::walk(to_print$path, ~{
   rmarkdown::render(.x)
-  pagedown::chrome_print(gsub("\\.Rmd", "\\.html", .x))
+  pagedown::chrome_print(.x)
 })
 
